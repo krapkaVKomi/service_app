90562ed52b63dc37e7e418ede38995f229af700b
Oleh <lg2031832@gmaol.com>
2023-07-18 04:59:00 +0300

    Create models

--- service/services/admin.py
+++ service/services/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from services.models import Service, Plan, Subscripthion
+
+
+# Register your models here.
+
+admin.site.register(Service)
+admin.site.register(Plan)
+admin.site.register(Subscripthion)
